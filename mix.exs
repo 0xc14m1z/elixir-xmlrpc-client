8d31cdf0cbb8f070fd2f4bd919e5554b1a79fc86
Luca Ramundo <luca@ramundo.info>
2016-05-31 16:01:37 +0200

    Add HTTPotion as dependency

--- mix.exs
+++ mix.exs
@@ -10,23 +10,11 @@ defmodule XmlrpcClient.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :httpotion]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:httpotion, "~> 2.2.0"}]
   end
 end
