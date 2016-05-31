39b773bc2191102f4279d8332dd0db9d950c2e55
Luca Ramundo <luca@ramundo.info>
2016-05-31 18:20:56 +0200

    Add the preferred cli to the project

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule XmlrpcClient.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     preferred_cli_env: [espec: :test]]
   end
 
   def application do
