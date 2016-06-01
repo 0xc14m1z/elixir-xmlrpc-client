6c3ac3558471d56a15fa568927af5be4f01f0517
Luca Ramundo <luca@ramundo.info>
2016-06-01 23:59:26 +0200

    Implement .call method

--- lib/xmlrpc_client.ex
+++ lib/xmlrpc_client.ex
@@ -1,7 +1,10 @@
 defmodule XMLRPC_Client do
-  
-  def call(client, method_name, params \\ []) do
+  alias XMLRPC_Client.Client
 
+  def call(client, method_name, params \\ []) when (client != nil and method_name != nil and method_name != "") do
+    request_body = %XMLRPC.MethodCall{method_name: method_name, params: params} |> XMLRPC.encode!
+    response = HTTPotion.post(Client.url(client), [body: request_body])
+    response.body |> XMLRPC.decode
   end
 
 end
