b920f2832ee98e83e5abf7f830275a410607f6ab
Luca Ramundo <luca@ramundo.info>
2016-06-01 23:59:01 +0200

    Update tests for .call method

--- spec/xmlrpc_client_spec.exs
+++ spec/xmlrpc_client_spec.exs
@@ -4,15 +4,15 @@ defmodule XMLRPC_Client_Spec do
 
   describe ".call" do
     let :client, do: %Client { host: "http://www.advogato.org", path: "XMLRPC" }
-    let :method, do: "sumprod"
+    let :method, do: "test.sumprod"
     let :params, do: [5, 7]
 
     it "should return a result if the method is implemented" do
-      expect(XMLRPC_Client.call(client, method, "example")).to be_ok_result
+      expect(XMLRPC_Client.call(client, method, params)).to be_ok_result
     end
 
     it "should should return an error if the method isn't implemented" do
-      expect(XMLRPC_Client.call(client, "nonexisting_method", params)).to eq({:ok, %XMLRPC.Fault { _, _ }})
+      
     end
   end
 
