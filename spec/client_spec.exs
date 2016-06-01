8ca30c7a39507bdbfebf33839d3a0b39abe6ebcf
Luca Ramundo <luca@ramundo.info>
2016-06-01 16:58:07 +0200

    Use alias for XMLRPC_Client.Client

--- spec/client_spec.exs
+++ spec/client_spec.exs
@@ -1,30 +1,31 @@
 defmodule XMLRPC_Client.ClientSpec do
+  alias XMLRPC_Client.Client
   use ESpec
 
   describe ".url" do
     it "should give nil if no parameters are given" do
-      client = %XMLRPC_Client.Client { }
-      expect(client).to eq(nil)
-    end
-
-    it "should give full url if all parameters are given" do
-      client = %XMLRPC_Client.Client { host: "www.example.com", path: "xmlrpc", port: 1234 }
-      expect(XMLRPC_Client.Client.url(client)).to eq("www.example.com:1234/xmlrpc")
-    end
-
-    it "should give the url without the port if it isn't given" do
-      client = %XMLRPC_Client.Client { host: "www.example.com", path: "xmlrpc" }
-      expect(XMLRPC_Client.Client.url(client)).to eq("www.example.com/xmlrpc")
-    end
-
-    it "should give the url without the path if it isn't given" do
-      client = %XMLRPC_Client.Client { host: "www.example.com", port: 1234 }
-      expect(XMLRPC_Client.Client.url(client)).to eq("www.example.com:1234")
+      client = %Client {}
+      expect(Client.url(client)).to eq(nil)
     end
 
     it "should give the url with only the hostname if path and port arent' given" do
-      client = %XMLRPC_Client.Client { host: "www.example.com" }
-      expect(XMLRPC_Client.Client.url(client)).to eq("www.example.com")
+      client = %Client { host: "www.example.com" }
+      expect(Client.url(client)).to eq("www.example.com")
+    end
+
+    it "should give the url without the port if it isn't given" do
+      client = %Client { host: "www.example.com", path: "xmlrpc" }
+      expect(Client.url(client)).to eq("www.example.com/xmlrpc")
+    end
+
+    it "should give the url without the path if it isn't given" do
+      client = %Client { host: "www.example.com", port: 1234 }
+      expect(Client.url(client)).to eq("www.example.com:1234")
+    end
+
+    it "should give full url if all parameters are given" do
+      client = %Client { host: "www.example.com", path: "xmlrpc", port: 1234 }
+      expect(Client.url(client)).to eq("www.example.com:1234/xmlrpc")
     end
   end
 end
