0c165ce15a24b689f593eafb13d2efa8ccb9caf6
Luca Ramundo <luca@ramundo.info>
2016-06-02 08:29:33 +0200

    Change test parameters for XMLRPC call

--- spec/xmlrpc_client_spec.exs
+++ spec/xmlrpc_client_spec.exs
@@ -4,16 +4,15 @@ defmodule XMLRPC_Client_Spec do
 
   describe ".call" do
     let :client, do: %Client { host: "http://www.advogato.org", path: "XMLRPC" }
-    let :method, do: "test.sumprod"
-    let :params, do: [5, 7]
+    let :method, do: "test.strlen"
+    let :params, do: "example"
 
     it "should return a result if the method is implemented" do
       expect(XMLRPC_Client.call(client, method, params)).to be_ok_result
     end
 
     it "should should return an error if the method isn't implemented" do
-      
+      expect(XMLRPC_Client.call(client, "nonexisting_method", params)).to be_error_result
     end
   end
-
 end
