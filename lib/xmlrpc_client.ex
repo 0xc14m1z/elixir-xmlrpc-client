99503a78196f03a1f5145a198763ee6a4797e240
Luca Ramundo <luca@ramundo.info>
2016-06-02 08:30:45 +0200

    Add the parse_response method which return :ok or :error

--- lib/xmlrpc_client.ex
+++ lib/xmlrpc_client.ex
@@ -4,7 +4,14 @@ defmodule XMLRPC_Client do
   def call(client, method_name, params \\ []) when (client != nil and method_name != nil and method_name != "") do
     request_body = %XMLRPC.MethodCall{method_name: method_name, params: params} |> XMLRPC.encode!
     response = HTTPotion.post(Client.url(client), [body: request_body])
-    response.body |> XMLRPC.decode
+    response.body |> XMLRPC.decode |> parse_response
+  end
+
+  defp parse_response({:ok, %XMLRPC.Fault { fault_code: error_code, fault_string: error_message }}) do
+    {:error, "#{error_code} - #{error_message}"}
+  end
+  defp parse_response({:ok, %XMLRPC.MethodResponse { param: response }}) do
+    {:ok, response}
   end
 
 end
