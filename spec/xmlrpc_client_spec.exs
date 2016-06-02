bf46e96153b4774c8f5fb0695ded27caf06dfc88
Luca Ramundo <luca@ramundo.info>
2016-06-02 08:33:32 +0200

    Change the name of the support struct from Client to Server

--- spec/xmlrpc_client_spec.exs
+++ spec/xmlrpc_client_spec.exs
@@ -1,18 +1,18 @@
 defmodule XMLRPC_Client_Spec do
-  alias XMLRPC_Client.Client
+  alias XMLRPC_Client.Server
   use ESpec
 
   describe ".call" do
-    let :client, do: %Client { host: "http://www.advogato.org", path: "XMLRPC" }
+    let :server, do: %Server { host: "http://www.advogato.org", path: "XMLRPC" }
     let :method, do: "test.strlen"
     let :params, do: "example"
 
     it "should return a result if the method is implemented" do
-      expect(XMLRPC_Client.call(client, method, params)).to be_ok_result
+      expect(XMLRPC_Client.call(server, method, params)).to be_ok_result
     end
 
     it "should should return an error if the method isn't implemented" do
-      expect(XMLRPC_Client.call(client, "nonexisting_method", params)).to be_error_result
+      expect(XMLRPC_Client.call(server, "nonexisting_method", params)).to be_error_result
     end
   end
 end
